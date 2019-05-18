aa39c4722cfca84784a82661d181edf550342463
Lex Aeterna <lexaeterna12321@gmail.com>
2019-05-18 20:10:54 +0200

    users route added / user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,10 +24,36 @@ router.post(
   ],
   async (req, res) => {
     const { name, email, password } = req.body;
-
+    console.log(req.body);
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      let fetchedUser = await User.findOne({ email });
+
+      if (fetchedUser) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const user = new User({
+        name,
+        email,
+        password
+      });
+
+      //   password encryption
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.status(200).json("User registered.");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
     }
   }
 );
