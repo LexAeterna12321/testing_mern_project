67ac8633e3e4c5fc764a86c9055bea4ffe6b9193
Lex Aeterna <lexaeterna12321@gmail.com>
2019-06-08 13:00:13 +0200

    init

--- server.js
+++ server.js
@@ -26,46 +26,46 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
-const server = app.listen(PORT, () =>
-  console.log(`Listening on PORT: ${PORT}`)
-);
-const io = socket(server);
+// const server = app.listen(PORT, () =>
+//   console.log(`Listening on PORT: ${PORT}`)
+// );
+// const io = socket(server);
 
-let typingUsers = [];
+// let typingUsers = [];
 
-io.on("connection", socket => {
-  socket.on("chat", function(data) {
-    io.sockets.emit("chat", JSON.parse(data));
-  });
+// io.on("connection", socket => {
+//   socket.on("chat", function(data) {
+//     io.sockets.emit("chat", JSON.parse(data));
+//   });
 
-  socket.on("typing", function(data) {
-    const parsedData = JSON.parse(data);
-    if (parsedData.typing) {
-      const filtered = typingUsers.filter(user => {
-        return user.userName === parsedData.userName;
-      });
-      filtered.length > 0 ? null : typingUsers.push(parsedData);
-    } else {
-      typingUsers = typingUsers.filter(
-        user => user.userName !== parsedData.userName
-      );
-    }
-    socket.broadcast.emit("typing", typingUsers);
-  });
+//   socket.on("typing", function(data) {
+//     const parsedData = JSON.parse(data);
+//     if (parsedData.typing) {
+//       const filtered = typingUsers.filter(user => {
+//         return user.userName === parsedData.userName;
+//       });
+//       filtered.length > 0 ? null : typingUsers.push(parsedData);
+//     } else {
+//       typingUsers = typingUsers.filter(
+//         user => user.userName !== parsedData.userName
+//       );
+//     }
+//     socket.broadcast.emit("typing", typingUsers);
+//   });
 
-  // socket.on("userConnect", function(data) {
-  //   const parsedData = JSON.parse(data);
+// socket.on("userConnect", function(data) {
+//   const parsedData = JSON.parse(data);
 
-  //   const userName = parsedData.userName;
-  //   console.log({ data, parsedData });
-  //   socket.broadcast.emit("userConnect", parsedData);
-  // });
+//   const userName = parsedData.userName;
+//   console.log({ data, parsedData });
+//   socket.broadcast.emit("userConnect", parsedData);
+// });
 
-  // socket.on("disconnect", function(data) {
-  //   const parsedData = JSON.parse(data);
-  //   const userName = parsedData.userName;
-  //   console.log({ data, parsedData });
-  //   console.log(`${userName} disconnected...`);
-  //   socket.broadcast.emit("userDisconnect", userName);
-  // });
-});
+// socket.on("disconnect", function(data) {
+//   const parsedData = JSON.parse(data);
+//   const userName = parsedData.userName;
+//   console.log({ data, parsedData });
+//   console.log(`${userName} disconnected...`);
+//   socket.broadcast.emit("userDisconnect", userName);
+// });
+// });
